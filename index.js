6bfca820422ccc08fd3f4902d4e748f89792910e
jbiehl88 <jcbiehl8@gmail.com>
2022-03-01 09:56:03 -0600

    starting code

---
+++ index.js
@@ -0,0 +1,26 @@
+// Game Object
+
+const gameState = {
+    players: ['x', 'o'],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
+  }
+
+
+// Write functions to maniuplate gameState --> maybe even a method?
+
+
+
+
+
+// put stuff on the screen
+
+
+
+
+
+
+// make them work together
