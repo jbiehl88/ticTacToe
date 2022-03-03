f5e563111604eaa513eeab639447764cbeec0873
jbiehl88 <jcbiehl8@gmail.com>
2022-03-03 17:42:56 -0600

    tie

--- index.js
+++ index.js
@@ -11,18 +11,15 @@ const gameState = {
   currentPlayer: "X",
 };
 
-// function computerPlay () {
-  
-// }
+// have our order chosen for us by the game
 
 function winCheck() {
   let array = gameState.board.flat();
   const winBanner = document.querySelector("#winBanner");
-  // console.log(winBanner, "banner");
   console.log(array);
   let winMessageX = "Congratulations! " + player1 + " wins!!!";
   let winMessageO = "Congratulations! " + player2 + " wins!!!";
-  let tieMessage = ""
+  let tieMessage = "Oh no! It was a tie. New game?"
   if (gameState.currentPlayer === "X") {
     if (array[0] === "X" && array[1] === "X" && array[2] === "X") {
       winBanner.innerText = winMessageX;
@@ -62,7 +59,6 @@ function winCheck() {
     }
   } else if (gameState.currentPlayer === "computer") {
     if (array[0] === "computer" && array[1] === "computer" && array[2] === "computer") {
-      console.log(winMessageO, "hello again");
       winBanner.innerText = winMessageO;
     } else if (array[3] === "computer" && array[4] === "computer" && array[5] === "computer") {
       winBanner.innerText = winMessageO;
@@ -79,6 +75,10 @@ function winCheck() {
     } else if (array[2] === "computer" && array[4] === "computer" && array[6] === "computer") {
       winBanner.innerText = winMessageO;
     }
+  } else if ((array[0] === (X || O || 'computer')) && (array[1] === (X || O || 'computer')) && (array[2] === (X || O || 'computer')) &&
+    (array[3] === (X || O || 'computer')) && (array[4] === (X || O || 'computer')) && (array[5] === (X || O || 'computer')) && 
+    (array[6] === (X || O || 'computer')) && (array[7] === (X || O || 'computer')) && (array[8] === (X || O || 'computer'))) {
+      winBanner.innerText = tieMessage
   }
 }
 
@@ -112,7 +112,6 @@ function newSpot() {
         currentCell = 'seven'
     } else if (row === 2 && column === 2){
         currentCell = 'eight'
-        console.log('hello im a message brought to you by Toyota')
     }
     const computerCell = document.getElementById(currentCell)
     computerCell.innerText = 'O'
@@ -125,30 +124,20 @@ function newSpot() {
 
 // // listeners
 board.addEventListener("click", function (event) {
-  // console.log(event.target.innerText.length, "checking this now");
   if (!event.target.innerText.length) {
-    // console.log(gameState.count);
     if (gameState.count % 2 === 1 && gameState.players[1] !== 'computer') {
       gameState.currentPlayer = gameState.players[1];
-      // console.log(event.target.id);
       gameState.board[event.target.id] = gameState.players[1];
       event.target.innerText = gameState.players[1];
 
     } else {
       gameState.currentPlayer = gameState.players[0];
-      // console.log(event.target.id);
       gameState.board[event.target.id] = gameState.players[0];
       event.target.innerText = gameState.players[0];
       if (gameState.players[1] === 'computer') {
         console.log('hello im a computer')
-        // winCheck()
         newSpot(event)
         console.log(gameState.board)
-        
-        // check if the board at those index value is null
-        // *******if gameState.count % 2 ===1 && player2 is not computer
-        // if null place computers move at index
-        // if !null find new index
       }
     }
     if (event.target.id === "zero") {
@@ -179,10 +168,6 @@ const pvp = document.querySelector("#twoPlayer");
 const comp = document.querySelector("#onePlayer");
 const reset = document.querySelector("#newGame");
 
-// comp.addEventListener('click', function(event) {
-
-// })
-
 reset.addEventListener("click", function (event) {
   gameState.board = [
     [null, null, null],
@@ -200,8 +185,6 @@ reset.addEventListener("click", function (event) {
   clearMessage.innerText = ""
 });
 
-// let playX = document.getElementsByClassName('textX')
-// let playO = document.getElementsByClassName('textO')
 let submitX = document.getElementsByClassName('submitX')
 let submitO = document.getElementsByClassName('submitO')
 let playerOneForm = document.getElementById('playerOne')
@@ -212,37 +195,22 @@ let playerO = document.querySelector('.playerO')
 playerOneForm.addEventListener('submit', function(event) {
   event.preventDefault()
   let playX = document.getElementsByClassName('textX')[0]
-  // console.log(playX.value)
   playerX.innerText = `X is: ${playX.value}`
 })
 
 playerTwoForm.addEventListener('submit', function(event) {
   event.preventDefault()
   let playO = document.getElementsByClassName('textO')[0]
-  // console.log(playX.value)
   playerO.innerText = `O is: ${playO.value}`
 })
 
-
-
-// put stuff on the screen
-
-
-// const computer = document.createElement('button')
-// computer.classList.add('computer')
-
-// make them work together
-
-// Write functions to manipulate gameState --> maybe even a method
-
-
 comp.addEventListener('click', function(event) {
+  let playerO = document.querySelector('.playerO')
+  playerO.innerText = 'O is: computer'
   gameState.players[1] = 'computer'
   console.log(gameState.players)
 })
-/* 
-****step 1 - add an event listener to computer button
-****step 2 -  reset the gameState.players[1] to computer
-step 3 - add checks for player2 and change the logic to if (player2 === computer) 
-step 4 - add random gen for computer moves
-*/
+
+pvp.addEventListener('click', function(event) {
+  location.reload()
+})
